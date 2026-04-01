3ba0a4bbffbdd3b2f4bdbd7edd2e084429e441fa
niyas <niyasx0011@gmail.com>
2026-04-01 12:53:37 +0400

    changed app titile

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Jayden - NIYAS ABDUL BASHEER",
+  title: "niyas.dev",
   description: "Portfolio website clone implementation.",
 };
 
